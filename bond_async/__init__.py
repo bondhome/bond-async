ea6a2f03a362ba28db5120de1b4c7c93bd4b124c
J. Nick Koston <nick@koston.org>
2023-03-25 09:36:04 -1000

    Use orjson to decode json
    Home Assistant switched to using orjson last year
    for performance reasons. This reduces startup/restart time as well

--- bond_async/__init__.py
+++ bond_async/__init__.py
@@ -1,10 +1,10 @@
 """Asynchronous Python wrapper library over Bond Local API."""
 
-from .bond import Bond
-from .bpup import BPUPSubscriptions, start_bpup
 from .action import Action, Direction
-from .device_type import DeviceType
+from .bond import Bond
 from .bond_type import BondType
+from .bpup import BPUPSubscriptions, start_bpup
+from .device_type import DeviceType
 
 __all__ = [
     "Bond",
